6e3ee64986109253eaf19ac8ac80d81daba1b1fc
Ergwynt <aday18005@gmail.com>
2024-10-22 00:30:05 +0100

    Mejora de bootrap

--- tasks/forms.py
+++ tasks/forms.py
@@ -7,3 +7,9 @@ class AddTaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('name', 'description', 'done', 'complete_before')
+
+
+class EditTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ('name', 'description')
--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,7 @@ from django.utils.text import slugify
 
 from tasks.models import Task
 
-from .forms import AddTaskForm
+from .forms import AddTaskForm, EditTaskForm
 
 
 def task_list(request):
@@ -20,9 +20,9 @@ def task_detail(request, slug):
 def add_task(request):
     if request.method == 'POST':
         if (form := AddTaskForm(request.POST)).is_valid():
-            post = form.save(commit=False)
-            post.slug = slugify(post.title)
-            post.save()
+            task = form.save(commit=False)
+            task.slug = slugify(task.name)
+            task.save()
             return redirect('tasks:task-list')
     else:
         form = AddTaskForm()
@@ -31,12 +31,25 @@ def add_task(request):
 
 
 def task_pending(request):
-    pass
+    tasks = Task.objects.all()
+    num_tasks_pending = tasks.filter(done=False).count()
+    task_pending = tasks.filter(done=False)
+    return render(
+        request,
+        './tasks/task/done_pending.html',
+        {'num_pendings': num_tasks_pending, 'task_pending': task_pending},
+    )
 
 
-def task_done(request, slug):
-    task = Task.objects.get(slug=slug)
-    return render(request, 'tasks/task/task_stage.html', dict(task=task))
+def task_done(request):
+    tasks = Task.objects.all()
+    num_tasks_pending = tasks.filter(done=True).count()
+    task_pending = tasks.filter(done=True)
+    return render(
+        request,
+        './tasks/task/done_pending.html',
+        {'num_pendings': num_tasks_pending, 'task_pending': task_pending},
+    )
 
 
 def task_toggle(request, slug):
@@ -49,9 +62,24 @@ def task_toggle(request, slug):
     return redirect('tasks:task-list')
 
 
-def task_delete(request):
-    pass
+def task_delete(request, slug):
+    task = Task.objects.get(slug=slug)
+    task.delete()
+    return redirect('tasks:task-list')
 
 
-def task_edit(request):
-    pass
+def task_edit(request, slug: str):
+    task = Task.objects.get(slug=slug)
+
+    if request.method == 'POST':
+        if (form := EditTaskForm(request.POST, instance=task)).is_valid():
+            task = form.save(commit=False)
+
+            task.slug = slugify(task.name)
+
+            task.save()
+
+            return redirect('tasks:task-list')
+    else:
+        form = EditTaskForm(instance=task)
+    return render(request, 'tasks/edit_task.html', dict(tasks=task, form=form))
